e1087747d1d3a6c3f1448845e8355c93f6826f89
Doran3007 <doran3007@gmail.com>
2021-06-15 22:17:21 +0300

    change index, add dark theme

--- project_football/settings.py
+++ project_football/settings.py
@@ -132,13 +132,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATICFILES_LOCATION = 'static'
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'project_football/static')
 ]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
 INTERNAL_IPS = [
